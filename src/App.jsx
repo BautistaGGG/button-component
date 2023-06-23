186e24c1a26c8b176e567e5651e0a1110f060586
unknown <gonzalezbautistagasto@gmail.com>
2023-06-23 12:27:13 -0300

    Creado el primer botón.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/no-unescaped-entities */
+// El diseño tiene 22 BOTONES, es decir, 22 REUSABLE-COMPONENTS
+import './App.css'
+import Button from "./components/Button"
+
+function App() {
+  return (
+    <main>
+        <Button text/>
+    </main>
+  )
+}
+
+export default App
